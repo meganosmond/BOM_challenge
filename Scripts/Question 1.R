3e7ffbd38d977ea02dcea0170097d7a6b436de31
Osmond <osm018@csiro.au>
2020-03-17 12:34:31 +1100

    Answer to Q1

--- Scripts/Question 1.R
+++ Scripts/Question 1.R
@@ -34,17 +34,11 @@ BOM_grouped_by_station
 summarise(
   BOM_grouped_by_station, n_days = n())
 
-Answer to Q1 <- 
+Answer_to_Q1 <- 
   separate(BOM_data, Temp_min_max, into = c("Temp_min", "Temp_max"), sep = "/") %>% 
   filter(Temp_min != "-" , Temp_max != "-", Rainfall != "-") %>% 
   group_by(Station_number) %>% 
   summarise (n_days = n())
 
 
-#Q2 Which month saw the lowest average daily temperature difference?
-# This question can be answered using just the BOM_data file.
-# In addition to the functions you used above, this question will need a mutate() to calculate the temperature diffence.
-# The temperature values are stored as characters after you have run separate() (see the <chr> in the second row if you print the data frame to the console). To be able to calculate the difference without an error, you will need to convert them to numeric values with as.numeric() first.
-# For rows that are missing a temperature measurement, the temperature difference will be NA. How will you deal with these in the rest of the analysis?
 
- 
